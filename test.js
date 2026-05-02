71c3f8072dcf8e92133a6c3efb4da60e301c5371
Rodriguez Carlos <Rodriguez15082002@gmail.com>
2026-05-02 15:18:34 -0500

    borrar todo

--- test.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk');
-
-console.log(chalk.blue("Iniciando pruebas de seguridad..."));
-console.log(chalk.yellow("Verificando colisión de claves (simulación)..."));
-
-setTimeout(() => {
-    console.log(chalk.green("✅ Prueba superada: Las rutinas de generación aseguran claves únicas."));
-}, 1000);
